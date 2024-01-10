322d29702945ea0e9b53c02e4f915f2aa69017e0
Luka Kavtaradze <Lk@gmail.com>
2024-01-11 00:51:16 +0400

    first details

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const body = document.querySelector("body")
+const toogle  = document.querySelector(".toogle")
+
+toogle.addEventListener("click",() => toogle.classList.toggle('active'))
+console.log(body,toogle);
+
+
